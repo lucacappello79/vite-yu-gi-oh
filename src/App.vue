5307615eaa72ff677c11f9f2c9c627934ae4e1c3
lucacappello79 <postt79@gmail.com>
2023-03-28 14:48:03 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<script>
+import AppHeader from "./components/AppHeader.vue"
+
+export default {
+  data() {
+    return {
+      viewTitle: true,
+    };
+  },
+
+  components: {
+    AppHeader,
+
+  },
+
+};
+</script>
+
+<template>
+  <AppHeader></AppHeader>
+</template>
+
+<style lang="scss"></style>
